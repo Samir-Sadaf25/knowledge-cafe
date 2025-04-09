27a82b15116ec471a23c5965331fa6c35058fd2d
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2025-04-09 23:39:53 +0600

    project initial setup done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Blogs from './componants/blogs/Blogs'
+import Navber from './componants/Navber/Navber'
+
+function App() {
+  return (
+    <>
+       <Navber></Navber>
+       <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
---
+++ src/componants/Navber/Navber.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navber = () => {
+    return (
+        <div>
+            <div class="navbar bg-base-300">
+                <button class="btn btn-ghost text-xl">daisyUI</button>
+            </div>
+        </div>
+    );
+};
+
+export default Navber; <div class="navbar bg-base-300">
+    <button class="btn btn-ghost text-xl">daisyUI</button>
+</div>
